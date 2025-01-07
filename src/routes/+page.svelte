3044ad7ee7695879b90159f579198cb986770b77
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-01-07 21:40:38 +0000

    Move login route to /login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
+<script>
+	import { enhance } from '$app/forms';
+
+	let { data } = $props();
+</script>
+
+<h1>Hi, {data.user.username}!</h1>
+<p>Your user ID is {data.user.id}.</p>
+<form method="post" action="?/logout" use:enhance>
+	<button>Sign out</button>
+</form>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
