7fdbfbbba0baea2127e52fea6534ff342b7d7044
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-01-07 23:59:42 +0000

    Refactor out LoginForm component

---
+++ src/lib/components/login/LoginForm.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { enhance } from '$app/forms';
+
+	let { form } = $props();
+</script>
+
+<div class="flex justify-center gap-4">
+    <form class="flex flex-col rounded-xl font-medium gap-8" method="post" action="?/login" use:enhance>
+        <div class="relative">
+            <input
+                id="username"
+                class="rounded-md text-black"
+                name="username"
+                placeholder=Username
+            />
+            <label for="username" class="p-2 focus:bg-sky-400"></label>
+        </div>
+        <div class="relative">
+            <input
+                id="password"
+                class="rounded-md text-black"
+                type="password"
+                name="password"
+                placeholder=Password
+            />
+            <label for="password" class="p-2 focus:bg-sky-400"></label>
+        </div>
+        <button class="p-2 hover:bg-slate-500 text-white rounded-md">Login</button>
+        <button class="p-2 hover:bg-slate-500 text-white rounded-md" formaction="?/register">Register</button>
+    </form>
+</div>
+
+<p class="flex justify-center text-red-500 font-medium text-xl">{form?.message ?? ''}</p>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,20 +1,9 @@
 <script>
-	import { enhance } from '$app/forms';
+	import LoginForm from '$lib/components/login/LoginForm.svelte';
 
 	let { form } = $props();
 </script>
 
-<h1 class="py-16 flex justify-center text-6xl text-white">Login/Register</h1>
-<form class="py-16 rounded-xl flex justify-center items-center gap-x-4 font-medium" method="post" action="?/login" use:enhance>
-	<label class="p-2 focus:bg-sky-400 text-white">
-		Username
-		<input class="rounded-md text-black" name="username" />
-	</label>
-	<label class="p-2 focus:bg-sky-400 text-white">
-		Password
-		<input class="rounded-md text-black" type="password" name="password" />
-	</label>
-	<button class="p-2 hover:bg-slate-500 text-white rounded-md">Login</button>
-	<button class="p-2 hover:bg-slate-500 text-white rounded-md" formaction="?/register">Register</button>
-</form>
-<p class="flex justify-center text-red-500 font-medium text-xl">{form?.message ?? ''}</p>
+<h1 class="py-16 flex justify-center text-6xl">Login/Register</h1>
+
+<LoginForm {form}></LoginForm>
